a690d5394543c081b5fb57802ffd5f321af44ad9
Artem <mobzik@inbox.ru>
2021-04-26 02:01:12 +0400

    feat jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -14,6 +15,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
   closeButton.addEventListener('click', switchModal); 
 
   
+});
+*/
+
+$(document).ready(function() {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close'),
+      closeButton = $('.okno');
+
+  modalBtn.on('click', function() {
+    modal.toggleClass('modal--visible')
+  });    
+
+  closeBtn.on('click', function() {
+    modal.toggleClass('modal--visible')
+  });   
+
+  closeButton.on('click', function() {
+    modal.toggleClass('modal--visible')
+  });   
+  
+
+});
+
+jQuery(document).ready(function() {
+  var btn = $('#button');  
+  $(window).scroll(function() {     
+    if ($(window).scrollTop() < 300) {
+       btn.addClass('show');
+     } else {
+       btn.removeClass('show');
+     }
+   });
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '100');
+  });
 });
 
 
